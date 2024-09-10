977637726b9af7dc2a44c9d2fe5889c81e2dfcfb
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-09-10 19:57:49 +0530

    Update install_hmmer3.1.sh

--- utils/install_hmmer3.1.sh
+++ utils/install_hmmer3.1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-tools="hmmscan"
-
+# install hmmer=3.1b1 from source
+echo 'installing hmmer=3.1b1 from source'
 function hmmer_install {
 wget -O hmmer-3.1b1.tar.gz http://eddylab.org/software/hmmer/hmmer-3.1b1.tar.gz
 tar zxvf hmmer-3.1b1.tar.gz ; rm -rf hmmer-3.1b1.tar.gz
@@ -9,7 +9,9 @@ cd hmmer-3.1b1
 ./configure
 make
 cd ..
-ln -f -s $PWD/hmmer-3.1b1/src/* ~/mambaforge/bin/
+ln -sf $PWD/hmmer-3.1b1/src/* ~/mambaforge/bin/
 }
 
 hmmer_install
+hmmer_path=`which hmmscan 2>/dev/null`
+echo 'hmmer=3.1b1 has been installed to ' ${hmmer_path}
