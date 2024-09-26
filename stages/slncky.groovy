6ce90188c7416f85371a0ea40043215e71edcd16
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-09-26 16:10:38 +0530

    Update slncky.groovy

--- stages/slncky.groovy
+++ stages/slncky.groovy
@@ -89,8 +89,8 @@ run_slncky = {
 
 ortholog_search = {
 	output.dir = slncky_dir
-	if (liftover != ""){ 
-    from("annotation.config", "Putative-lnc-nptcs.bed") produce(rel_sp_name+".orthologs.top.txt"){
+	if (liftover != ""){
+	from("annotation.config", "Putative-lnc-nptcs.bed") produce(rel_sp_name+".orthologs.top.txt"){
             exec """
             source ${Activate} cpc2-cpat-slncky
             $slncky -n $threads -c $input1 $input2 $slncky_ortho_options $org_name $output.prefix.prefix.prefix
