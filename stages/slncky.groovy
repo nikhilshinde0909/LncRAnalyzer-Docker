1e22f0addda6a729601a38366b7527f27676fddf
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-09-26 16:09:19 +0530

    Update slncky.groovy

--- stages/slncky.groovy
+++ stages/slncky.groovy
@@ -88,17 +88,17 @@ run_slncky = {
 }
 
 ortholog_search = {
-    output.dir = slncky_dir
-    from("annotation.config", "Putative-lnc-nptcs.bed") produce("${rel_sp_name}.orthologs.top.txt") {
-        if (liftover != "") {
+	output.dir = slncky_dir
+	if (liftover != ""){ 
+    from("annotation.config", "Putative-lnc-nptcs.bed") produce(rel_sp_name+".orthologs.top.txt") {
             exec """
             source ${Activate} cpc2-cpat-slncky
-            ${slncky} -n ${threads} -c ${input1} ${input2} ${slncky_ortho_options} ${org_name} ${output.prefix.prefix.prefix}
+            $slncky -n $threads -c $input1 $input2 $slncky_ortho_options $org_name $output.prefix.prefix.prefix
             """
-        } else {
-            exec "echo 'Liftover files not provided for conservation studies'"
+    		} 
+	} else {
+	exec "echo 'Liftover files not provided for conservation studies'"
         }
-    }
 }
 
 slncky_run = segment { ref_genome_bed + fasta_index + annotation_config + putative_lnc_npcts_bed + run_slncky + ortholog_search }
