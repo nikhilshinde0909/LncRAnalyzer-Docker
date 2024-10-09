2f9f89fd9290d77c6402304dade9f87fa35838fa
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-10-09 13:03:53 +0530

    Add files via upload

--- scripts/find_species_in_ensembl.py
+++ scripts/find_species_in_ensembl.py
@@ -4,18 +4,35 @@ import glob, os, sys
 from pybiomart import Server
 import pandas as pd
 
+def main(ensembl_name):
+    server = Server(host='http://plants.ensembl.org')
+    version = server.list_marts()
+    
+    version = version[version['display_name'].str.contains('Ensembl Plants Genes')]
+    if version.empty:
+        print("No Ensembl Plants Genes mart found.")
+        sys.exit(1)
 
-ensembl_name = sys.argv[1]
+    mart = server['plants_mart']
+    df = mart.list_datasets()
+    
+    df2 = df[df['name'].str.contains(ensembl_name, case=False) | 
+              df['display_name'].str.contains(ensembl_name, case=False)]
+    
+    if df2.empty:
+        print(f"No datasets found matching '{ensembl_name}'.")
+        sys.exit(1)
 
-#Ensembl mart
-server = Server(host='http://plants.ensembl.org')
-version = server.list_marts()
-version = version[version['display_name'].str.contains('Ensembl Plants Genes')]
-version = str(version.iloc[0]['display_name']).split(" ")[2]
-mart = server['plants_mart'] 
-df = mart.list_datasets()
-df2 = df[df['name'].str.contains(str(ensembl_name))]
-if df2.empty == True:
-    df2 = df[df['display_name'].str.contains(str(ensembl_name))]
-sys.stdout.write(str(df2.iloc[0]['name']).split("_")[0])
-sys.exit(0)
+    matching_names = df2['display_name']
+#.apply(lambda x: " ".join(x.split()[:2])).unique()
+    print("Following datasets are available at ensemble plants for " + ensembl_name)
+    for name in matching_names:
+        print(name)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: find_species_in_ensembl.py <org_name>")
+        sys.exit(1)
+    
+    ensembl_name = sys.argv[1]
+    main(ensembl_name)
