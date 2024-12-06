8613e51f12fe7500b5ddf7910535f3f718024440
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-12-06 15:28:57 +0530

    Update CPAT.groovy

--- stages/CPAT.groovy
+++ stages/CPAT.groovy
@@ -30,9 +30,9 @@ build_hexamer_table = {
     output.dir = CPAT_dir
     if (!file(hexamer_table) || !file(logit_model)){
         from(org_name+".cds.fa") produce(org_name+"_hexamer.TSV"){
-            exec """
-                $python2 $make_hexamer -c $input -n $known_lncRNAs_FA > $output
-            """
+        exec """
+	$python2 $make_hexamer -c $input -n $known_lncRNAs_FA > $output
+        """
         }
     } else {
         exec "echo 'No need to build hexamer table'"
@@ -43,9 +43,9 @@ build_logit_model = {
     output.dir = CPAT_dir
     if (!file(hexamer_table) || !file(logit_model)){
         from(org_name + "_hexamer.TSV", org_name + ".cds.fa") produce(org_name + ".make_logitModel.r"){
-            exec """
-                $python2 $logit_model -x $input1 -c $input2 -n $known_lncRNAs_FA -o $output.prefix
-            """
+	exec """
+        $python2 $logit_model -x $input1 -c $input2 -n $known_lncRNAs_FA -o $output.prefix
+	"""
         }
     } else {
         exec "echo 'No need to build logit model'"
@@ -72,15 +72,15 @@ run_CPAT = {
     output.dir = CPAT_dir
     if (file(hexamer_table) || file(logit_model)){
         from("Putative.lnc_NPCTs.fa") produce("CPAT_output.TSV"){
-            exec """
-                $python2 $CPAT -x $hexamer_table -g $input -d $logit_model $CPAT_options -o $output
-            """
+	exec """
+        $python2 $CPAT -x $hexamer_table -g $input -d $logit_model $CPAT_options -o $output
+	"""
         }
     } else {
         from(org_name+"_hexamer.TSV", "Putative.lnc_NPCTs.fa") produce("CPAT_output.TSV"){
-            exec """
-                $python2 $CPAT -x $input1 -g $input2 -d ${output.dir}/${org_name}.logit.RData $CPAT_options -o $output
-            """
+        exec """
+	$python2 $CPAT -x $input1 -g $input2 -d ${output.dir}/${org_name}.logit.RData $CPAT_options -o $output
+        """
         }
     }
 }
