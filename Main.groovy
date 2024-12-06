35cd8000aa630cb436cd25dd99cf98ee41f338f5
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-12-06 18:06:33 +0530

    Update Main.groovy

--- Main.groovy
+++ Main.groovy
@@ -7,7 +7,7 @@
 
 VERSION="1.00"
 def printHelp() {
-    println "Usage: groovy main.groovy [options]"
+    println "Usage: bpipe run Main.groovy [options]"
     println "Options:"
     println "  < config_file >       Specify the config file eg. data.groovy"
     println "  --supporting_species  Show supporting species for LncRNAlyzer"
