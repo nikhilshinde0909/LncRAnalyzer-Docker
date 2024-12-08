7e58c015769295676e8226901f0d3f67be01d2e2
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-12-08 11:25:54 +0530

    Update rnasamba.groovy

--- stages/rnasamba.groovy
+++ stages/rnasamba.groovy
@@ -41,7 +41,7 @@ rnasamba_classify = {
 
 rnasamba_final_lnc_RNAs = {
 	output.dir=rnasamba_dir
-	from("Putative_lnc_NPCTs.rnasamba.TSV") produce("final_lnc_RNAs-rnasamba.TSV","final_lnc_RNAs-rnasamba.list"){
+	from("Putative.lnc_NPCTs.rnasamba.TSV") produce("final_lnc_RNAs-rnasamba.TSV","final_lnc_RNAs-rnasamba.list"){
 	exec """
 	grep -w 'noncoding' $input > $output1 ;
 	grep -w 'noncoding' $input|cut -f1|sed 1,1d > $output2
@@ -51,7 +51,7 @@ rnasamba_final_lnc_RNAs = {
 
 rnasamba_final_NPCTs = {
 	output.dir=rnasamba_dir
-	from("Putative_lnc_NPCTs.rnasamba.TSV") produce("final_NPCTs-rnasamba.TSV","final_NPCTs-rnasamba.list"){
+	from("Putative.lnc_NPCTs.rnasamba.TSV") produce("final_NPCTs-rnasamba.TSV","final_NPCTs-rnasamba.list"){
 	exec """
 	grep -w 'coding' $input > $output1 ;
 	grep -w 'coding' $input|cut -f1|sed 1,1d > $output2
@@ -61,7 +61,7 @@ rnasamba_final_NPCTs = {
 
 rnasamba_extract_fasta = {
 	output.dir=rnasamba_dir
-	from("Putative.lnc_NPCTs.fa","final_lnc_RNAs-rnasamba.list","inal_NPCTs-rnasamba.list") produce("final_lnc_RNAs-rnasamba.fa","final_NPCTs-rnasamba.fa"){
+	from("Putative.lnc_NPCTs.fa","final_lnc_RNAs-rnasamba.list","final_NPCTs-rnasamba.list") produce("final_lnc_RNAs-rnasamba.fa","final_NPCTs-rnasamba.fa"){
 	exec """
 	${seqtk} subseq $input1 $input2 > $output1 ;
 	${seqtk} subseq $input1 $input3 > $output2
@@ -69,7 +69,6 @@ rnasamba_extract_fasta = {
 	}
 }
 
-rnasamba_train_and_classify = segment { rnasamba_classify + 
+rnasamba_train_and_classify = segment { rnasamba_train + rnasamba_classify + 
 				rnasamba_final_lnc_RNAs + rnasamba_final_NPCTs + 
 				rnasamba_extract_fasta }
-
