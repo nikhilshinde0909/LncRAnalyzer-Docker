e4a696b37d933be7f782733337fc4b721d4c8653
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-12-16 10:28:53 +0530

    Delete Models/CPAT/Sorghum_bicolor.make_logitModel.r

--- Models/CPAT/Sorghum_bicolor.make_logitModel.r
+++
@@ -1,4 +0,0 @@
-data <- read.table(file="CPAT/Sorghum_bicolor.feature.xls",sep="\t",header=T)
-attach(data)
-mylogit <- glm(Label ~ mRNA + ORF + Fickett + Hexamer, family=binomial(link="logit"), na.action=na.pass)
-save.image("CPAT/Sorghum_bicolor.logit.RData")
